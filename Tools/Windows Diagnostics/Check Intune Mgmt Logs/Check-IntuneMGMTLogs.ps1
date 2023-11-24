70d0a3160552ea482cd373d49f3ed1f98f5cac51
WillH <75690476+BassJamm@users.noreply.github.com>
2023-11-24 20:44:31 +0000

    Update Tools/Windows Diagnostics/Check Intune Mgmt Logs/Check-IntuneMGMTLogs.ps1

--- Tools/Windows Diagnostics/Check Intune Mgmt Logs/Check-IntuneMGMTLogs.ps1
+++ Tools/Windows Diagnostics/Check Intune Mgmt Logs/Check-IntuneMGMTLogs.ps1
@@ -1,32 +1,41 @@
-<# Script checks the intune Win logs for applicaiton download times. #>
-$logFileContent = Get-Content -Path (Get-ChildItem -Path C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\ -Filter "IntuneManagement*.log").FullName
-
-###### Identify each row. ######
-$filteredLogs = $logFileContent | Where-Object { $_ -match "<!\[LOG\[\[(.+)<time=(.+)>" }
-
-###### Identify the column data. ######
-$regexDate = "[0-9]{2}-[0-9]{2}-[0-9]{4}"
-$regexTime = "[0-9]{2}:[0-9]{2}:[0-9]{2}"
-
-function Get-Pattern ( $pattern ) {
-
-    switch -Regex  ( $pattern ) {
-        '<!\[LOG\[\[StatusService\]\sDownloading\sapp\s\(id\s=\s[\w]{8}-[\w]{4}-[\w]{4}-[\w]{4}-[0-9A-Fa-f]{12},\sname\s(.+?)\)' { "Downloading App" } # Finds application downloading notifications.
-        '<!\[LOG\[\[StatusService\]\sProgress\sreport\sis\sbeing\sskipped\sas\sit\sis\snot\sassociated\swith\san\sIntune user.(.+)>' { "Skipping App" } # Finds the downloaded application skipped notifcation that follows after.
+function installIntent ($intent) {
+    switch ($intent) {
+        '3' { "Required" }
+        '0' { "None" }
         Default {}
     }
 }
 
-############ Search for Application Installs ############
-$output = foreach ($row in $filteredLogs) {
-
-    [PSCustomObject]@{
-        Date    = ( $row | Select-String -Pattern $regexDate ).Matches.Value
-        Time    = ( $row | Select-String -Pattern $regexTime ).Matches.Value
-        Message = ( $row | Select-String -Pattern "\s(.*?)\]" ).Matches.Value -replace "]", ""
+############ Switch to denote if app is applicable to machine ############
+function installApplicable ($applicable) {
+    switch ($applicable) {
+        '1' { "Yes" }
+        '0' { "No" }
+        Default {}
     }
+} 
+
+############ Search for Application Installs ############
+foreach ($row in $filteredLogs) {
+
+    $policyData = ($row | Select-String -Pattern "\s=\s(.+)]]LOG]").Matches.Value -replace "=\s", "" -replace "\]LOG\]", "" | ConvertFrom-Json
+
+    $outputAppInfo = foreach ($item in $policyData) {
+
+        [PSCustomObject]@{
+            Id              = $item.Id
+            Name            = $item.Name
+            Install_Intent  = installintent -intent $item.Intent
+            TargetType      = $item.TargetType
+            IsItApplicable  = installApplicable -applicable $item.Targeted
+            SetupFile_Info  = $item.SetUpFilePath
+            Install_CMD     = $item.InstallCommandLine
+            Uninstall_CMD   = $item.UninstallCommandLine
+            Notifications   = $item.ToastState
+            RebootBehaviour = $item.RebootEx -join ","
+            Detection_Rule  = ($item.DetectionRule | ConvertFrom-Json).DetectionText | ConvertFrom-Json
+        }
+    } 
 }
 
-$output | Out-File C:\Temp\IntuneLogs\IntuneWinOutput.txt
-Write-Host "Log Exported to C:\Temp\IntuneLogs\IntuneWinOutput.txt"
-$output | Where-Object { ( $_.Message -Like "*Downloading app (id*" ) -or ( $_.Message -like "*application poller starts.*" ) -or ( $_.Message -like "*application poller stopped.*" ) } | Format-Table -AutoSize
+$outputAppInfo | Sort-Object Name | Format-Table * -AutoSize
