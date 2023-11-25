0d6a2184cbf4f413e3ae6a98913797a0e58433eb
WillH <75690476+BassJamm@users.noreply.github.com>
2023-11-25 09:30:10 +0000

    Create Check-device-checkins.ps1

---
+++ Tools/Windows Diagnostics/Intune Management/Check-device-checkins.ps1
@@ -0,0 +1,20 @@
+# Function used to re-run the value through the switch.
+function InTuneAction ($Source) {
+    switch ($source) {
+        'UserAgentOrigin: (0x1)' { "Account Settings Initiated Check-in." }
+        'UserAgentOrigin: (0x5)' { "Account Settings Initiated Check-in." }
+        'UserAgentOrigin: (0x2)' { "Device Initiated Check-in." }
+        'UserAgentOrigin: (0x4)' { "Device Initated Check-in at user logon." }
+        'UserAgentOrigin: (0x8)' { "Company Portal Initiated Check-in." }
+        'UserAgentOrigin: (0x7)' { "Intune Initiated Check-in." }
+        'UserAgentOrigin: (0x8)' { "Company Portal Initiated Check-in." }
+        Default {}
+    }
+} 
+# Splatting for the properties otherwise it'd be mega long. 
+$properties = @(
+    'TimeCreated',
+    @{l = 'Source'; e = { InTuneAction -Source ( $_.message | Select-String 'UserAgentOrigin:\s\(0x.\)' ).Matches.Value } }
+)
+# Command grabs the events and the sources.
+Get-WinEvent -FilterHashTable @{ LogName = 'Microsoft-Windows-DeviceManagement-Enterprise-Diagnostics-Provider/Admin' ; id = 208; ; StartTime = (get-date).AddDays(-1) } | Select-Object $properties
